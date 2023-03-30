dc323b5915293ddcb25ed17afb851992f969a5b9
NaufalJohana <naufaljohana@gmail.com>
2023-03-30 16:44:56 +0700

    langkah algoritma ke 11

--- QuickSort_119/QuickSort_119.cpp
+++ QuickSort_119/QuickSort_119.cpp
@@ -82,3 +82,10 @@ void q_short(int low, int high)
 			mov_count++;
 		}
 	}
+	// j now containt the index of the last element in the sorted list
+	if (low < j)										//langkah algoritma no. 11
+	{
+		//move the pivot to its correct position in the list
+		swap(low, j);
+		mov_count++;
+	}
