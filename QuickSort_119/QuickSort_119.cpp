73a07661aa980189f0e6927a3e6aa70d39745783
NaufalJohana <naufaljohana@gmail.com>
2023-03-30 16:14:48 +0700

    membuat variable dan memasukan nilai nilai ke array

--- QuickSort_119/QuickSort_119.cpp
+++ QuickSort_119/QuickSort_119.cpp
@@ -3,7 +3,6 @@ using namespace std;
 
 
 int arr[20];				// array of integers to hold values
-
 int cmp_count = 0;			// number of comparasion
 int mov_count = 0;			// number of data movements
 int n;
